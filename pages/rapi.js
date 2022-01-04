ddd93cad49a493ebe0ebc0a952c542bd8f057dd7
zhangsan <zhangsan@test.com>
2022-01-04 21:03:57 +0900

    服务器端页面保护

--- pages/rapi.js
+++ pages/rapi.js
@@ -2,6 +2,8 @@ import Counter from '../components/widgets/Counter'
 import MainLayout from '../components/layout/MainLayout'
 import RApi from '../components/RApi'
 
+import { getSession } from 'next-auth/react'
+
 export default function RApiPage() {
     return (
         <MainLayout title='Restful API 测试'>
@@ -11,4 +13,18 @@ export default function RApiPage() {
             </main>
         </MainLayout>
     )
+}
+export async function getServerSideProps(context) {
+    const session = await getSession({ req: context.req })
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth/login',
+                permanent: false,
+            }
+        }
+    }
+    return {
+        props: { session },
+    }
 }
